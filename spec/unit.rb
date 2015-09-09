2244ce1e835f345c03f2bd428a4d2635a640314b
Won-Min Chang <wonminchang@gmail.com>
2015-09-09 21:12:35 +0000

    rspec

---
+++ spec/unit.rb
@@ -0,0 +1,11 @@
+class Unit 
+  attr_reader :health_points
+  def initialize
+    @health_points = health_points
+    @attack_points = attack_points
+  end
+
+  def health_points
+    return
+  end
+end
